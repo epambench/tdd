1fe0650dcee70353de3d5754dc9afe2994cd0cc2
Vladimir_Brayko <Vladimir_Brayko@epam.com>
2014-12-28 14:34:52 +0100

    refactored test

--- tdd-sample/src/test/java/com/tdd/sample/WordsCounterTest.java
+++ tdd-sample/src/test/java/com/tdd/sample/WordsCounterTest.java
@@ -8,41 +8,49 @@ import org.junit.Test;
 public class WordsCounterTest {
 
 	private WordsCounter wordsCounter;
+	public static final String TEST_WORD = "word";
+	public static final String EMPTY_WORD = "";
+	public static final String SPACE = " ";
+	public static final String TEST_SYMBOL = "a";
 
 	@Before
 	public void setUp() throws Exception {
 		wordsCounter = new WordsCounter();
 	}
-	
+
 	@Test
 	public void testGetCountForNull() {
 		wordsCounter.count(null);
 		assertEquals(0, wordsCounter.getCount(null));
-		assertEquals(0, wordsCounter.getCount(""));
-		assertEquals(0, wordsCounter.getCount(" "));
-		assertEquals(0, wordsCounter.getCount("word"));
+		assertEquals(0, wordsCounter.getCount(EMPTY_WORD));
+		assertEquals(0, wordsCounter.getCount(SPACE));
+		assertEquals(0, wordsCounter.getCount(TEST_WORD));
+		assertEquals(0, wordsCounter.getCount(TEST_SYMBOL));		
 	}
 
 	@Test
 	public void testGetCountForEmptyWord() {
 		wordsCounter.count("");
-		assertEquals(1, wordsCounter.getCount(""));
-		assertEquals(0, wordsCounter.getCount(" "));
-		assertEquals(0, wordsCounter.getCount("word"));
-		wordsCounter.count(" ");
-		wordsCounter.count(" ");
-		wordsCounter.count(" ");
-		assertEquals(3, wordsCounter.getCount(" "));
+		assertEquals(1, wordsCounter.getCount(EMPTY_WORD));
+		assertEquals(0, wordsCounter.getCount(SPACE));
+		assertEquals(0, wordsCounter.getCount(TEST_WORD));
+		assertEquals(0, wordsCounter.getCount(TEST_SYMBOL));
+		int counter = 4;
+		for (int i = 0; i < counter; i++) {
+			wordsCounter.count(SPACE);
+		}
+		assertEquals(counter, wordsCounter.getCount(SPACE));
 	}
 
 	@Test
 	public void testGetCountForEmptyCounter() {
 		assertEquals(0, wordsCounter.getCount(null));
-		assertEquals(0, wordsCounter.getCount(""));
-		assertEquals(0, wordsCounter.getCount(" "));
-		assertEquals(0, wordsCounter.getCount("word"));
+		assertEquals(0, wordsCounter.getCount(EMPTY_WORD));
+		assertEquals(0, wordsCounter.getCount(SPACE));
+		assertEquals(0, wordsCounter.getCount(TEST_WORD));
+		assertEquals(0, wordsCounter.getCount(TEST_SYMBOL));
 	}
-	
+
 	@Test
 	public void testGetCountForMultipleWords() {
 		wordsCounter.count("test word");
@@ -56,14 +64,13 @@ public class WordsCounterTest {
 
 	@Test
 	public void testGetCountForDifferentWords() {
-		wordsCounter.count("word");
-		wordsCounter.count("word");
+		wordsCounter.count(TEST_WORD);
+		wordsCounter.count(TEST_WORD);
+		assertEquals(2, wordsCounter.getCount(TEST_WORD));
 		wordsCounter.count(" word");
-		wordsCounter.count(" word ");
-		assertEquals(2, wordsCounter.getCount("word"));
-		assertEquals(0, wordsCounter.getCount("words"));
 		assertEquals(1, wordsCounter.getCount(" word"));
+		wordsCounter.count(" word ");
 		assertEquals(1, wordsCounter.getCount(" word "));
+		assertEquals(0, wordsCounter.getCount("words"));		
 	}
 }
-
